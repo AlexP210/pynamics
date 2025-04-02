ac2f4b00a2e3b6778e0d028db07004403a0d7503
Alex Pleava <61223491+AlexP210@users.noreply.github.com>
2025-04-01 22:05:01 -0400

    More tests (#16)
    * Added test for RevoluteDCMotor
    
    * Updated coverege format to markdown

--- pynamics/tests/test_dynamics.py
+++ pynamics/tests/test_dynamics.py
@@ -9,6 +9,7 @@ import trimesh
 import pynamics.kinematics.topology as topo
 import pynamics.kinematics.joint as joint
 import pynamics.dynamics as dynamics
+import pynamics.math.linalg as linalg
 from pynamics.sim import Sim
 import pynamics.constants as const
 from pynamics.visualizer import Visualizer
@@ -48,8 +49,8 @@ class TestDynamics(unittest.TestCase):
         plt.clf()
         plt.cla()
         plt.close()
-
-    def make_free_cube_topology(self):
+    
+    def make_cube_topology(self, cube_joint:joint.Joint):
         cube = topo.Body(
             mass_properties_model=trimesh.load(
                 file_obj=os.path.join(const.HOME, "models", "common", "Cube.obj"),
@@ -67,7 +68,7 @@ class TestDynamics(unittest.TestCase):
             parent_frame_name="Identity",
             child_body=cube.copy(),
             child_body_name="Cube",
-            joint=joint.FreeJoint(),
+            joint=cube_joint,
         )
 
         visualizer = Visualizer(
@@ -86,7 +87,7 @@ class TestDynamics(unittest.TestCase):
     def test_buoyancy(self):
         warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
 
-        topology, visualizer = self.make_free_cube_topology()
+        topology, visualizer = self.make_cube_topology(joint.FreeJoint())
         topology.joints["Cube"].set_configuration(np.matrix([1, 0, 0, 0, 0, 0, -500]).T)
         sim = Sim(
             topology=topology,
@@ -131,7 +132,7 @@ class TestDynamics(unittest.TestCase):
 
     def test_gravity(self):
         warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
-        topology, visualizer = self.make_free_cube_topology()
+        topology, visualizer = self.make_cube_topology(joint.FreeJoint())
         # topology.joints["Cube"].set_configuration(np.matrix([1/np.sqrt(4),1/np.sqrt(4),1/np.sqrt(4),1/np.sqrt(4),5,0,0]).T)
         topology.joints["Cube"].set_configuration(np.matrix([1, 0, 0, 0, 5, 0, 0]).T)
         sim = Sim(
@@ -165,7 +166,7 @@ class TestDynamics(unittest.TestCase):
 
     def test_drag(self):
         warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
-        topology, visualizer = self.make_free_cube_topology()
+        topology, visualizer = self.make_cube_topology(joint.FreeJoint())
         topology.joints["Cube"].set_configuration_d(np.matrix([0, 0, 0, 0.1, 0, 0]).T)
         topology.joints["Cube"].set_configuration(np.matrix([1, 0, 0, 0, 0, 0, -10]).T)
         sim = Sim(
@@ -213,8 +214,8 @@ class TestDynamics(unittest.TestCase):
 
     def test_spring(self):
         warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
-        topology, visualizer = self.make_free_cube_topology()
-        topology.joints["Cube"].set_configuration_d(np.matrix([0, 0, 0, 0, 0, -0.1]).T)
+        topology, visualizer = self.make_cube_topology(joint.TranslationJoint())
+        topology.joints["Cube"].set_configuration_d(np.matrix([0, 0, -0.1]).T)
         sim = Sim(
             topology=topology,
             body_dynamics={
@@ -246,7 +247,7 @@ class TestDynamics(unittest.TestCase):
         k = m
         omega = np.sqrt(k / m)
         x_expected = (-0.1 / omega) * np.sin(omega * t)
-        x = sim.data_history["Cube / Position 6"]
+        x = sim.data_history["Cube / Position 2"]
 
         self.save_plot_artifact(
             time=t,
@@ -259,4 +260,161 @@ class TestDynamics(unittest.TestCase):
         self.assertTrue(
             (abs(x - x_expected) < const.EPSILON).all(),
             msg="Spring simulates correctly on a free body.",
+        )
+
+    def test_joint_damping(self):
+        warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
+        topology, visualizer = self.make_cube_topology(joint.RevoluteJoint(0))
+        topology.joints["Cube"].set_configuration_d(np.matrix([0.1,]).T)
+        sim = Sim(
+            topology=topology,
+            joint_dynamics={
+                "damping": dynamics.JointDamping(
+                    damping_factor=0.5,
+                    joint_names=["Cube"]
+                )
+            },
+        )
+        sim.simulate(delta_t=5, dt=0.01)
+        visualizer.add_sim_data(sim)
+        if not os.getenv("GITHUB_ACTIONS"):
+            visualizer.animate(
+                save_path=os.path.join(const.HOME, "tests", "test_joint_damping.mp4")
+            )
+
+        t = np.array(sim.data_history["Time"])
+        # x_dd = -(k/m)*x_d
+        # v_d = -(k/m)*v
+        # (1/v)*dv = -(k/m)*dt
+        # ln(v) + C = -(k/m)*t
+        # v(t) = Ae^(-(k/m)*t)
+        # => v(0) = A
+        # x_d(t) = v(0)*e^(-kt/m)
+        # x(t) = (-v(0)m/k)*e^(-k*t/m) + B
+        # => B = x(0) + v(0)m/k
+        # x(t) = (-v(0)m/k)*e^(-k*t/m) + x(0) + v(0)m/k
+        x_hat = np.matrix([1,0,0]).T
+        I = (x_hat.T @ topology.bodies["Cube"].inertia_matrix @ x_hat)[0,0]
+        x_expected = (0.1 * I / 0.5) * (1 - np.exp(-0.5 * t / I))
+        x = sim.data_history["Cube / Position 0"]
+
+        self.save_plot_artifact(
+            time=t,
+            value=x,
+            expected_value=x_expected,
+            value_name="Joint Position",
+            test_name="test_joint_damping",
+        )
+
+        self.assertTrue(
+            (abs(x - x_expected) < const.EPSILON).all(),
+            msg="Joint damping simulates correctly on a free body.",
+        )
+
+    def test_fixed_joint(self):
+        warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
+        topology, visualizer = self.make_cube_topology(joint.FixedJoint())
+        sim = Sim(
+            topology=topology,
+            body_dynamics={
+                "gravity": dynamics.Gravity(
+                    g=9.81,
+                    direction=np.matrix([0,0,1]).T,
+                    body_names=["Cube"]
+                )
+            },
+        )
+        sim.simulate(delta_t=5, dt=0.01)
+        visualizer.add_sim_data(sim)
+        if not os.getenv("GITHUB_ACTIONS"):
+            visualizer.animate(
+                save_path=os.path.join(const.HOME, "tests", "test_joint_damping.mp4")
+            )
+
+        t = np.array(sim.data_history["Time"])
+        # x_dd = -(k/m)*x_d
+        # v_d = -(k/m)*v
+        # (1/v)*dv = -(k/m)*dt
+        # ln(v) + C = -(k/m)*t
+        # v(t) = Ae^(-(k/m)*t)
+        # => v(0) = A
+        # x_d(t) = v(0)*e^(-kt/m)
+        # x(t) = (-v(0)m/k)*e^(-k*t/m) + B
+        # => B = x(0) + v(0)m/k
+        # x(t) = (-v(0)m/k)*e^(-k*t/m) + x(0) + v(0)m/k
+        self.assertTrue(
+            list(sim.data_history.keys()) == ["Time"]
+        )
+
+    def test_revolute_motor(self):
+        warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
+        topology, visualizer = self.make_cube_topology(joint.RevoluteJoint(0))
+        sim = Sim(
+            topology=topology,
+            joint_dynamics={
+                "damping": dynamics.RevoluteDCMotor(
+                    joint_name="Cube",
+                    electromotive_constant=10,
+                    resistance=0.1,
+                    inductance=0.1,
+                    voltage=100
+                )
+            },
+        )
+        sim.simulate(delta_t=5, dt=0.01)
+        visualizer.add_sim_data(sim)
+        if not os.getenv("GITHUB_ACTIONS"):
+            visualizer.animate(
+                save_path=os.path.join(const.HOME, "tests", "test_revolute_motor.mp4")
+            )
+
+        ts = np.array(sim.data_history["Time"])
+
+        # https://ctms.engin.umich.edu/CTMS/index.php?example=MotorSpeed&section=SystemModeling
+        # x = [w, i].T
+        # A = [-b/J,  K/J 
+        #      -K/L, -R/L]
+        # B = [0, V/L].T
+        # x_d = Ax + B
+        # => x(t) = e^(At)*C + x_p
+        # => x_p = e^(At)*int(0, t, e^(-At)*B)
+        # => x_p = e^(At) * (-inv(A)*e^(-At)*B)
+        # x(t) = e^(At)*C +  e^(At) * (-inv(A)*e^(-At)*B)
+        # => x(0) = 0
+        # => C = inv(A)*B
+        # x(t) = e^(At)*inv(A)*B +  e^(At) * (-inv(A)*e^(-At)*B)
+        x_hat = np.matrix([1,0,0]).T
+        J = (x_hat.T @ topology.bodies["Cube"].inertia_matrix @ x_hat)[0,0] # inertia
+        b = 0 # viscous damping
+        K = 10 # Electromotive constant
+        L = 0.1 #inductance
+        R = 0.1 # resistance
+        V = 100 # Voltage
+        A = np.matrix([
+            [-b/J, K/J],
+            [-K/L, -R/L]
+        ])
+        inv_A = np.linalg.inv(A)
+        B = np.matrix([0, V/L]).T
+        x_expected = np.zeros(ts.size)
+
+        for t_idx in range(len(ts)):
+            t = ts[t_idx]
+            exp_At = linalg.matrix_exponential(A*t)
+            neg_exp_At = linalg.matrix_exponential(-A*t)
+
+            x_expected[t_idx] = (exp_At@inv_A@(np.eye(*A.shape) - neg_exp_At)@B)[0,0]
+        x = sim.data_history["Cube / Velocity 0"]
+
+        self.save_plot_artifact(
+            time=ts,
+            value=x,
+            expected_value=x_expected,
+            value_name="Joint Position",
+            test_name="test_revolute_motor",
+        )
+
+        self.assertTrue(
+            (abs(x - x_expected) < 0.005).all(),
+            msg="RevoluteDCMotor simulates correctly on a free body.",
         )
